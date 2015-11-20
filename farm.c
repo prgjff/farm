38b61643a60642b9b6f7bf728d908a5079e382f1
prgjff <prgjff@gmail.com>
2015-11-20 14:11:02 +0400

    Добавил получение данных с датчика

--- farm.c
+++ farm.c
@@ -1,10 +1,20 @@
+#include <wiringPi.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <getopt.h>
 #include <time.h>
 #ifdef _WIN32
 #include <Windows.h>
 #endif
+
+#include "locking.h"
+
+#define MAXTIMINGS 85
+#define MAX_MEASURE 5;
 
 struct measure
 {
@@ -15,6 +25,7 @@ struct measure
 	float Tmin = -100;
 	float Tmax = -100;
 	int timeDelay = -1;
+	static int DHTPIN = 7;	
 
 void printHelp(void)
 {
@@ -75,16 +86,112 @@ void heater(char active)
 		printf("Обогрев выключен\n");
 }
 
-int mi = 0;
+static uint8_t sizecvt(const int read)
+{
+  if (read > 255 || read < 0)
+  {
+    printf("Invalid data from wiringPi library\n");
+    exit(EXIT_FAILURE);
+  }
+  return (uint8_t)read;
+}
+
+static char read_dht22_dat(struct measure *m)
+{
+  char result = 0;
+  uint8_t laststate = HIGH;
+  uint8_t counter = 0;
+  uint8_t j = 0, i;
+  int dht22_dat[5] = {0,0,0,0,0};
+
+  int lockfd = open_lockfile(LOCKFILE);
+
+  if (wiringPiSetup () == -1)
+    exit(EXIT_FAILURE);
+
+  if (setuid(getuid()) < 0)
+  {
+    perror("Dropping privileges failed\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // pull pin down for 18 milliseconds
+  pinMode(DHTPIN, OUTPUT);
+  digitalWrite(DHTPIN, HIGH);
+  delay(10);
+  digitalWrite(DHTPIN, LOW);
+  delay(18);
+  // then pull it up for 40 microseconds
+  digitalWrite(DHTPIN, HIGH);
+  delayMicroseconds(40); 
+  // prepare to read the pin
+  pinMode(DHTPIN, INPUT);
+
+  // detect change and read data
+  for ( i=0; i< MAXTIMINGS; i++) {
+    counter = 0;
+    while (sizecvt(digitalRead(DHTPIN)) == laststate) {
+      counter++;
+      delayMicroseconds(1);
+      if (counter == 255) {
+        break;
+      }
+    }
+    laststate = sizecvt(digitalRead(DHTPIN));
+
+    if (counter == 255) break;
+
+    // ignore first 3 transitions
+    if ((i >= 4) && (i%2 == 0)) {
+      // shove each bit into the storage bytes
+      dht22_dat[j/8] <<= 1;
+      if (counter > 16)
+        dht22_dat[j/8] |= 1;
+      j++;
+    }
+  }
+
+  // check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
+  // print it out if data is good
+  if ((j >= 40) && 
+      (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF)) ) {
+        float t, h;
+        h = (float)dht22_dat[0] * 256 + (float)dht22_dat[1];
+        h /= 10;
+        t = (float)(dht22_dat[2] & 0x7F)* 256 + (float)dht22_dat[3];
+        t /= 10.0;
+        if ((dht22_dat[2] & 0x80) != 0)  t *= -1;
+
+    m->temp = t;
+    m->humidity = h;
+    printf("Humidity = %.2f %% Temperature = %.2f *C \n", h, t );
+    result = 1;
+  }
+  else
+  {
+    printf("Data not good, skip\n");
+    result = 0;
+  }
+
+  delay(1500);
+  close_lockfile(lockfd);
+  
+  return result; 
+}
 
 struct measure getMeasure()
 {
+	int indexMeasure = 0;
 	struct measure result;
-	if (mi > 100)	result.temp = mi - 100;
-	else if (mi > 50) result.temp = 100 - mi;
-	else result.temp = mi;
-	result.humidity = 25.5;
-	mi++;
+
+	while (read_dht22_dat() == 0) && (indexMeasure < MAX_MEASURE)
+  {
+     delay(1000); // wait 1sec to refresh
+     indexMeasure++;
+  }
+
+	//result.temp = 10;
+	//result.humidity = 25.5;
 	return result;
 }
 
